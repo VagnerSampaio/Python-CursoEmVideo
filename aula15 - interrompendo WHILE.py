2e7bca803c3cd1b552abd5173b64c76bd827c979
João Vagner de Alencar Sampaio <vagnerdsampaio@gmail.com>
2026-07-19 10:40:40 -0300

    Python - Curso em Vídeo

---
+++ aula15 - interrompendo WHILE.py
@@ -0,0 +1,22 @@
+cont = 1
+
+while True:
+    print(cont)
+    cont += 1
+    if cont == 10:
+        break
+
+s = 0
+while True:
+    n = int(input("Digite um número: "))
+    if n == 999:
+        break
+    s += n
+
+print("A soma vale {}".format(s))
+print(f"A soma vale {s}")
+
+nome = "Vagner"
+idade = 41
+salario = 13000.00
+print(f"O {nome:-^20} tem {idade} anos e ganha R${salario:.2f}")
